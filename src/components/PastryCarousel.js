00ce71b7a01f11394877595cbffadd82982a43ef
SylvainRousset <sylvaindebisca@hotmail.fr>
2024-09-08 12:03:17 +0200

    mediaquery

--- src/components/PastryCarousel.js
+++ src/components/PastryCarousel.js
@@ -1,70 +1,15 @@
 import React, { useState } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import '../Styles/PastryCarousel.scss';
-import PastryModal from './PastryModal'; // Import de la modal
+import { Swiper, SwiperSlide } from 'swiper/react'; // Importer Swiper et SwiperSlide
+import { Navigation, Pagination } from 'swiper/modules'; // Importer les modules Navigation et Pagination
+import 'swiper/css'; // Importation des styles de base
+import 'swiper/css/navigation'; // Importation des styles pour la navigation
+import 'swiper/css/pagination'; // Importation des styles pour la pagination
+import '../Styles/PastryCarousel.scss'; // Vos styles personnalisés
+import PastryModal from './PastryModal'; // Importer la modal
 
 function PastryCarousel({ pastries }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedPastry, setSelectedPastry] = useState(null);
-  const [startPosition, setStartPosition] = useState({ x: 0, y: 0 }); // Coordonnées de départ du clic
-
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 400,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false,
-    swipeToSlide: true,
-    touchMove: true,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
-  // Enregistrer la position de la souris/touch au début de l'interaction
-  const handleMouseDownOrTouchStart = (e) => {
-    const x = e.touches ? e.touches[0].clientX : e.clientX;
-    const y = e.touches ? e.touches[0].clientY : e.clientY;
-    setStartPosition({ x, y });
-  };
-
-  // Comparer la position de la souris/touch au moment de la fin pour voir si c'est un glissement ou un clic
-  const handleMouseUpOrTouchEnd = (e, pastry) => {
-    const x = e.changedTouches ? e.changedTouches[0].clientX : e.clientX;
-    const y = e.changedTouches ? e.changedTouches[0].clientY : e.clientY;
-
-    const deltaX = Math.abs(x - startPosition.x);
-    const deltaY = Math.abs(y - startPosition.y);
-
-    // Seuil pour différencier un glissement d'un clic (ajustez ce seuil si nécessaire)
-    if (deltaX < 5 && deltaY < 5) {
-      openModal(pastry);
-    }
-  };
 
   const openModal = (pastry) => {
     setSelectedPastry(pastry);
@@ -78,22 +23,48 @@ function PastryCarousel({ pastries }) {
 
   return (
     <div className="pastry-carousel">
-      <Slider {...settings}>
+      <Swiper
+        modules={[Navigation, Pagination]} // Ajouter la pagination et la navigation comme modules
+        spaceBetween={10} // Espace entre les slides
+        slidesPerView={1} // Nombre de slides visibles en même temps
+        breakpoints={{
+        
+          340: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+          1424: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+          2000: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        
+        
+        pagination={{ clickable: true }} // Ajouter une pagination cliquable
+        navigation // Activer la navigation avec les boutons précédent/suivant
+      >
         {pastries.map((pastry, index) => (
-          <div
-            key={index}
-            className="pastry-item"
-            onMouseDown={handleMouseDownOrTouchStart}
-            onMouseUp={(e) => handleMouseUpOrTouchEnd(e, pastry)}
-            onTouchStart={handleMouseDownOrTouchStart} // Pour les appareils tactiles
-            onTouchEnd={(e) => handleMouseUpOrTouchEnd(e, pastry)} // Pour les appareils tactiles
-          >
-            <img src={pastry.image_url} alt={pastry.title} />
-            <h3>{pastry.title}</h3>
-            <p>{pastry.price !== undefined ? pastry.price.toFixed(2) : "Prix non disponible"} €</p>
-          </div>
+          <SwiperSlide key={index} onClick={() => openModal(pastry)}>
+            <div className="pastry-item">
+              <img src={pastry.image_url} alt={pastry.title} />
+              <h3>{pastry.title}</h3>
+              <p>{pastry.price !== undefined ? pastry.price.toFixed(2) : "Prix non disponible"} €</p>
+            </div>
+          </SwiperSlide>
         ))}
-      </Slider>
+      </Swiper>
 
       {/* Modal pour afficher les détails du dessert */}
       <PastryModal
